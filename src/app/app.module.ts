d46b46ad7d9e8b2ebdf506823cfb4ab93858f8bd
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-04-21 13:13:51 +0300

    21.04.2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import {FormsModule,ReactiveFormsModule} from "@angular/forms"
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,7 +14,10 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import {ToastrModule} from "ngx-toastr";
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
-import { ProductAddedComponent } from './components/product-added/product-added.component'
+import { ProductAddedComponent } from './components/product-added/product-added.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +28,8 @@ import { ProductAddedComponent } from './components/product-added/product-added.
     VatAddedPipe,
     FilterPipePipe,
     CartSummaryComponent,
-    ProductAddedComponent
+    ProductAddedComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +42,9 @@ import { ProductAddedComponent } from './components/product-added/product-added.
     }),
     BrowserAnimationsModule  
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
